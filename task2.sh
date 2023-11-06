7aa9bfee7a7fc512148d9b666f2611cdc6654527
mandrake <perseptron@gmail.com>
2023-11-06 18:20:25 +0200

    composing json

--- task2.sh
+++ task2.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
 INPUT=$1
+OUTPUT=${1%.*}".json"
 function header(){
-  re='\[(.*?)\].*1..([0-9]+)'
+  re='\[ (.*?) \].*1..([0-9]+)'  # [ Asserts Samples ], 1..7 tests
   if [[ $line =~ $re ]]; then
     title="${BASH_REMATCH[1]}"
     test_num="${BASH_REMATCH[2]}"
   else
     echo "something wrong with header"
   fi
-  echo $title $test_num
 }
 
 function body(){
   #echo $line
-  re='^(ok|not ok).*\b([0-9]+) (.*),.*\b([0-9]+)ms'
+  re='^(ok|not ok).*\b([0-9]+) (.*),.*\b([0-9]+)ms'  # ok  3  expecting command fails (the same as above, bats way), 23ms
   if [[ $line =~ $re ]]; then
-    result="${BASH_REMATCH[1]}"
-    num="${BASH_REMATCH[2]}"
-    message="${BASH_REMATCH[3]}"
-    time="${BASH_REMATCH[4]}"
+    num+=("${BASH_REMATCH[2]}")
+    message+=("${BASH_REMATCH[3]}")
+    time+=("${BASH_REMATCH[4]}")
+    if [[ "${BASH_REMATCH[1]}" =~ "not" ]]; then
+      result+=("false")
+    else
+      result+=("true")
+    fi
   else
     echo "something wrong with body"
   fi
-  echo "test# $num completed in $time""ms and result is $result because - $message "
 }
 
 function overall(){
-  re='^([0-9]+).*,.*\b([0-9]+).*,.*\b([0-9]+.[0-9]+)%,.*\b([0-9]+)ms'
+  re='^([0-9]+).*,.*\b([0-9]+).*,.*\b([0-9]+.[0-9]+)%,.*\b([0-9]+)ms'  # 5 (of 7) tests passed, 2 tests failed, rated as 71.43%, spent 136ms
   if [[ $line =~ $re ]]; then
     success="${BASH_REMATCH[1]}"
     fails="${BASH_REMATCH[2]}"
@@ -35,7 +38,22 @@ function overall(){
   else
     echo "something wrong with overall"
   fi
-  echo "success: $success, fails: $fails, rate: $rates, time: $total_time"
+  json_compose
+}
+
+function json_compose(){
+  echo "{\"testName\": \"$title\", \"tests\": [" > $OUTPUT
+  for (( i=0; i < "${#num[@]}"; i++ )) ;do
+    echo "{ \"name\": \"${message[$i]}\", \"status\": \"${result[$i]}\", \"duration\": \"${time[$i]}ms\" }" >> $OUTPUT
+    if (( $i != ${#num[@]} -1 ));then
+       echo "," >> $OUTPUT
+    fi
+  done
+  echo "], \"summary\": {\"success\": $success, \"failed\": $fails, \"rating\": $rates, \"duration\": \"$total_time""ms\" } }" >> $OUTPUT
+  unset message
+  unset result
+  unset time
+  unset num
 }
 
 
