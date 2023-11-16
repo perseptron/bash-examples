c0c76a56fcad69fdd2906546955c2d29b7cac875
mandrake <perseptron@gmail.com>
2023-11-16 17:28:26 +0200

    little space fixes with \s

--- task2.sh
+++ task2.sh
@@ -2,7 +2,7 @@
 INPUT=$1
 OUTPUT=${1%.*}".json"
 function header(){
-  re='\[ (.*?) \].*1..([0-9]+)'  # [ Asserts Samples ], 1..7 tests
+  re='\[\s*(.*\s*.*) \].*1..([0-9]+)'  # [ Asserts Samples ], 1..7 tests
   if [[ $line =~ $re ]]; then
     title="${BASH_REMATCH[1]}"
     test_num="${BASH_REMATCH[2]}"
@@ -12,7 +12,7 @@ function header(){
 }
 
 function body(){
-  re='^(ok|not ok).*\b([0-9]+) (.*),.*\b([0-9]+)ms'  # ok  3  expecting command fails (the same as above, bats way), 23ms
+  re='^(ok|not ok).*\b([0-9]+)\s+(.*),.*\b([0-9]+)ms'  # ok  3  expecting command fails (the same as above, bats way), 23ms
   if [[ $line =~ $re ]]; then
     num+=("${BASH_REMATCH[2]}")
     message+=("${BASH_REMATCH[3]}")
@@ -28,7 +28,7 @@ function body(){
 }
 
 function overall(){
- re='([0-9]+).*, ([0-9]+).*,.* ([0-9]+(\.[0-9]+)?)%,.* ([0-9]+)ms'  # 5 (of 7) tests passed, 2 tests failed, rated as 71.43%, spent 136ms
+ re='([0-9]+).*,\s+([0-9]+).*,.*\s+([0-9]+(\.[0-9]+)?)%,.* ([0-9]+)ms'  # 5 (of 7) tests passed, 2 tests failed, rated as 71.43%, spent 136ms
  if [[ $line =~ $re ]]; then
     success="${BASH_REMATCH[1]}"
     fails="${BASH_REMATCH[2]}"
